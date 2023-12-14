822b3c94fb00f6ddd7ed114786f32b282f4641dc
Your Username <parhambarati32@gmail.com>
2023-12-14 10:17:47 -0500

    complete landing page for camping website

---
+++ components/Button.tsx
@@ -0,0 +1,21 @@
+import Image from "next/image";
+
+type ButtonProps = {
+    type: 'button' | 'submit';
+    title: string;
+    icon?:string;
+    varient: 'btn_dark_green' | 'btn_green' | 'btn_white_text' | 'btn_white' | 'btn_dark_green_outline'
+    full? : boolean
+}   
+
+const Button = ({type , title , icon , varient , full} : ButtonProps) => {
+  return (
+    <button
+    className={`flexCenter gap-3 rounded-full border ${varient} ${ full && 'w-full' }  `}
+        type={type}
+
+    > {icon && <Image src={icon} alt={title} width={24} height={24}   /> } <label className="bold-16 whitespace-nowrap cursor-pointer">  {title} </label> </button>
+  )
+}
+
+export default Button
